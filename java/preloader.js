88422c81bf1418e74031991ef22d16e5b1802a72
Mmlbt <nastia.pienkova69@gmail.com>
2020-06-28 09:23:13 +0300

    init commit

---
+++ java/preloader.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+window.onload = function(){
+	setTimeout(function(){
+		let preloader = document.getElementById('preloader');
+	preloader.style.display = 'none';
+	}, 1000);	
+}
